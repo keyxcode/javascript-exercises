f5a975d64bd50e3d27281218ca0514bfc289cb0e
keyxcode <keyxcode@gmail.com>
2023-01-25 16:46:49 +0700

    finish ex1 -> 6

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reversedString = "";
 
+    for (let i = stringLength - 1; i >= 0; i--) {
+        reversedString += string[i]; 
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    let newArray = [];
 
+    inputArray.forEach(e => {
+        if (!args.includes(e)) {
+            newArray.push(e);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
