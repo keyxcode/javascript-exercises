3bf4a17bb4eb473cadf649c4384fe1b1de229652
keyxcode <keyxcode@gmail.com>
2023-01-27 16:05:50 +0700

    finish ex10 to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(order) {
+    order = parseInt(order);
+    if (order < 0) return "OOPS";
 
+    if (order === 1 || order === 2) return 1;
+    else {
+        return fibonacci(order - 1) + fibonacci(order - 2);
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        let currentAge = getAge(currentPerson);
+        let oldestAge = getAge(oldestPerson);
+        return (oldestAge > currentAge) ? oldestPerson : currentPerson;
+    })
 };
+
+function getAge(person) {
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath;
+    if (!death) death = new Date().getFullYear();
+
+    return death - birth; 
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
