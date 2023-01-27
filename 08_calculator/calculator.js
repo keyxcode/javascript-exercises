3b5ff746681c17eb51af3e05e8423ed5a969ca89
keyxcode <keyxcode@gmail.com>
2023-01-27 13:34:49 +0700

    finish ex8, work on ex9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let num of array) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let num of array) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	fact = 1;
+  if (num === 0) return fact;
+  for (let i = 1; i <= num; i ++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+function isCharacterALetter(char) {
+    return char.toLowerCase() != char.toUpperCase()
+}
 
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const charArray = string.split("");
+    let arrLength = charArray.length;
+
+    for (let i = 0; i < arrLength; i++) {
+        if (!isCharacterALetter(charArray[i])) continue;
+        let j = arrLength - i - 1;
+            console.log(charArray[i]);
+            console.log(charArray[j]);
+            if (!isCharacterALetter(charArray[j])) continue;
+            if (charArray[i] != charArray[j]) return false;
+    }
+
+    return true;
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
